b5f8237a0f646eb353659f61d997b8036f5f2734
JosephEmer05 <ej.tarlengco@gmail.com>
2024-09-21 21:18:25 +0800

    seph

--- src/App.js
+++ src/App.js
@@ -1,21 +1,44 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
-import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap'; 
 import Home from './components/Home';
 import './styles/App.css';
 
 function App() {
   return (
     <Router>
-      <div>
-        <Navbar />
-        <Routes> 
-          <Route path="/" element={<Home />} />
-          <Route path="/locations" element={<div>Locations Page (Placeholder)</div>} />
-          <Route path="/contact" element={<div>Contacts Page (Placeholder)</div>} />
-          <Route path="/login" element={<div>Login/Registration Page (Placeholder)</div>} />
-        </Routes>
-      </div>
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <Container>
+          <LinkContainer to="/">
+            <Navbar.Brand>Narra-Yon</Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <LinkContainer to="/">
+                <Nav.Link>Home</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/locations">
+                <Nav.Link>Locations</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/contact">
+                <Nav.Link>Contact</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <Nav.Link>Login/Register</Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/locations" element={<div>Locations Page (Placeholder)</div>} />
+        <Route path="/contact" element={<div>Contacts Page (Placeholder)</div>} />
+        <Route path="/login" element={<div>Login/Registration Page (Placeholder)</div>} />
+      </Routes>
     </Router>
   );
 }
