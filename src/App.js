f2a30cb9b90cffd6b3961b6a5343454af68e5e5a
Mentsao <167274459+Mentsao@users.noreply.github.com>
2024-10-10 03:54:36 +0800

    More updates

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Route, Routes } from 'react-router-dom';
+import { Navbar, Nav } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import Home from './components/Home';
 import Contact from './components/Contact';
@@ -9,22 +9,24 @@ import Reservations from './components/Reservations';
 import './styles/App.css';
 import logo from './narralogo.png';
 
-
 function App() {
   return (
-    <Router>
+    <>
       <Navbar bg="white" variant="white" expand="lg" className="navbar-size">
-  <Container>
-    <div className="logo-container">
-      <LinkContainer to="/">
-        <Navbar.Brand>
-          <img src={logo} alt="Narra-Yon Logo" />
-        </Navbar.Brand>
-      </LinkContainer>
-    </div>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
-      <Nav>
+        <div className="navbar-content">
+          {/* Logo on the left */}
+          <LinkContainer to="/">
+            <Navbar.Brand className="navbar-logo-container">
+              <img src={logo} alt="Narra-Yon Logo" className="navbar-logo" />
+            </Navbar.Brand>
+          </LinkContainer>
+
+          {/* Navbar Toggle for mobile view */}
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+          {/* Navigation Links on the right */}
+          <Navbar.Collapse id="basic-navbar-nav" className="navbar-collapse">
+            <Nav className="navbar-links">
               <LinkContainer to="/">
                 <Nav.Link>Home</Nav.Link>
               </LinkContainer>
@@ -34,12 +36,15 @@ function App() {
               <LinkContainer to="/contact">
                 <Nav.Link>Contact Us</Nav.Link>
               </LinkContainer>
+              <LinkContainer to="/menu">
+                <Nav.Link>Menu</Nav.Link>
+              </LinkContainer>
               <LinkContainer to="/login">
                 <Nav.Link className="login-button">Log-In</Nav.Link>
               </LinkContainer>
             </Nav>
           </Navbar.Collapse>
-        </Container>
+        </div>
       </Navbar>
 
       <Routes>
@@ -49,7 +54,7 @@ function App() {
         <Route path="/contact" element={<Contact />} />
         <Route path="/login" element={<div>Log-In (Placeholder)</div>} />
       </Routes>
-    </Router>
+    </>
   );
 }
 
